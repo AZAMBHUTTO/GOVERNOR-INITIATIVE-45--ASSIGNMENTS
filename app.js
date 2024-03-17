065779f6d9d71cb5e393ac9146335399e637ad01
AZAMBHUTTO <160306543+AZAMBHUTTO@users.noreply.github.com>
2024-03-17 15:00:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+var man = "George Bernard Shaw once said,";
+var againsaid = "Progress is immpossible without change ,those who cannot change their minds cannot change anything";
+console.log(man);
+console.log(againsaid);
---
+++ app.ts
@@ -0,0 +1,4 @@
+let man:string="George Bernard Shaw once said,";
+let againsaid:string="Progress is immpossible without change ,those who cannot change their minds cannot change anything";
+console.log(man) 
+console.log(againsaid)
